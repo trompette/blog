d8ea5045e9808d9bf029214236d042861317f3d7
Benoît Merlet <benoit.merlet@gmail.com>
2013-12-18 18:58:21 +0100

    Use pythonic code

--- blog.py
+++ blog.py
@@ -6,10 +6,9 @@ import os
 
 logging.basicConfig(level=logging.DEBUG)
 
-engine\
-    .Engine(pages_dir=os.path.join(os.getcwd(), 'pages'),
-            posts_dir=os.path.join(os.getcwd(), 'posts'),
-            templates_dir=os.path.join(os.getcwd(), 'templates'),
-            web_dir = os.path.join(os.getcwd(), 'web'),
-            logger=logging.getLogger('blog-engine'))\
-    .start()
+e = engine.Engine(pages_dir=os.path.join(os.getcwd(), 'pages'),
+                  posts_dir=os.path.join(os.getcwd(), 'posts'),
+                  templates_dir=os.path.join(os.getcwd(), 'templates'),
+                  web_dir = os.path.join(os.getcwd(), 'web'),
+                  logger=logging.getLogger('blog-engine'))
+e.start()
