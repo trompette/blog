2cea26146bbdb36bc4c3b6fa1f8fbca8540e9f4a
Benoît Merlet <benoit.merlet@gmail.com>
2013-12-30 17:19:06 +0100

    Code refactoring

---
+++ engine/highlighting.py
@@ -0,0 +1,32 @@
+from pygments import highlight
+from pygments.formatters import get_formatter_by_name
+from pygments.lexers import get_lexer_by_name, guess_lexer
+from pygments.util import ClassNotFound
+from logging import getLogger
+
+logger = getLogger('blog-engine')
+
+
+def highlight_code(code, lang):
+    code = cleanup_code(code)
+
+    try:
+        lexer = get_lexer_by_name(lang, stripall=False)
+    except ClassNotFound:
+        logger.warning('Unknown Pygments language %s', lang)
+        lexer = guess_lexer(code)
+
+    return highlight(code, lexer, get_formatter_by_name('html'))
+
+
+def cleanup_code(code):
+    # removing empty ending lines
+    code = code.rstrip()
+    # replacing tabs by spaces
+    code = code.expandtabs(4)
+    # fixing indentation
+    lines = code.splitlines()
+    indent = min(map(lambda s: len(s)-len(s.lstrip(' ')) if s else float('inf'), lines))
+    code = '\n'.join(map(lambda s: s[indent:] if s else '', lines))
+
+    return code
--- engine/templating.py
+++ engine/templating.py
@@ -1,10 +1,9 @@
+from highlighting import highlight_code
 from logging import getLogger
 from jinja2 import Environment, FileSystemLoader
 from jinja2.ext import Extension, Markup
 from jinja2.nodes import CallBlock, Const
-from pygments import highlight
 from pygments.formatters import get_formatter_by_name
-from pygments.lexers import get_lexer_by_name, guess_lexer
 from pygments.util import ClassNotFound
 from yaml import load
 
@@ -12,37 +11,22 @@ logger = getLogger('blog-engine')
 
 
 class HighlightExtension(Extension):
-    tags = set(['highlight'])
+    tags = {'highlight'}
 
     def parse(self, parser):
         lineno = parser.stream.next().lineno
-        lang = Const(None) if parser.stream.current.test('block_end') else parser.parse_expression()
-        call = self.call_method('highlight_code', [lang])
+        lang = Const(None)
+        if not parser.stream.current.test('block_end'):
+            lang = parser.parse_expression()
+        call = self.call_method('_highlight', [lang])
         body = parser.parse_statements(['name:endhighlight'], drop_needle=True)
 
         return CallBlock(call, [], [], body).set_lineno(lineno)
 
-    def highlight_code(self, lang, caller):
-        code = self.cleanup_code(caller())
-        try:
-            lexer = get_lexer_by_name(lang, stripall=False)
-        except ClassNotFound:
-            logger.warning('Unknown Pygments language %s', lang)
-            lexer = guess_lexer(code)
+    def _highlight(self, lang, caller):
+        code = Markup(caller()).unescape()
 
-        return highlight(Markup(code).unescape(), lexer, get_formatter_by_name('html'))
-
-    def cleanup_code(self, code):
-        # removing empty ending lines
-        code = code.rstrip()
-        # replacing tabs by spaces
-        code = code.expandtabs(4)
-        # fixing indentation
-        lines = code.splitlines()
-        indent = min(map(lambda s: len(s)-len(s.lstrip(' ')) if s else float('inf'), lines))
-        code = '\n'.join(map(lambda s: s[indent:] if s else '', lines))
-
-        return code
+        return highlight_code(code, lang)
 
 
 def pygments_style(style):
